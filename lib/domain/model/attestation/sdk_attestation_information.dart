6b0dcb0a79374368371df1634e055a3cb4790442
Laszlo Domonkos <laszlo.domonkos@nevis.net>
2025-10-13 15:22:04 +0200

    NEVISACCESSAPP-6971: Adapt changes done to FidoUafAttestationInformation

--- lib/domain/model/attestation/sdk_attestation_information.dart
+++ lib/domain/model/attestation/sdk_attestation_information.dart
@@ -4,19 +4,29 @@ class SdkAttestationInformation {
   final bool onlySurrogateBasic;
   final bool onlyDefault;
   final bool strict;
+  final bool strongBox;
 
   SdkAttestationInformation.onlySurrogateBasic()
       : onlySurrogateBasic = true,
         onlyDefault = false,
-        strict = false;
+        strict = false,
+        strongBox = false;
 
   SdkAttestationInformation.onlyDefault()
       : onlySurrogateBasic = true,
         onlyDefault = true,
-        strict = false;
+        strict = false,
+        strongBox = false;
 
   SdkAttestationInformation.strict()
       : onlySurrogateBasic = true,
         onlyDefault = true,
-        strict = true;
+        strict = true,
+        strongBox = false;
+
+  SdkAttestationInformation.strictStrongBox()
+      : onlySurrogateBasic = true,
+        onlyDefault = true,
+        strict = true,
+        strongBox = true;
 }
